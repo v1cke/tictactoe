d007527f185cc6838b9bf3b6a86b89e5adbe19c1
v1cke <v1cke1982@gmail.com>
2022-05-06 23:59:13 +0200

    0506-3

--- script.js
+++ script.js
@@ -5,20 +5,20 @@ let winnersound = new Audio('youwon.mp3')
 
 function fillShape(id) {
     if (!fields[id]) {
-    if (currentShape == 'cross') {
-        currentShape = 'circle';
-        document.getElementById('p2').classList.remove('active-player');
-        document.getElementById('p1').classList.add('active-player');
-        document.getElementById(`circle-${id}`).classList.remove('d-none');
-    } else {
-        currentShape = 'cross';
-        document.getElementById('p1').classList.remove('active-player');
-        document.getElementById('p2').classList.add('active-player');
-        document.getElementById(`cross-${id}`).classList.remove('d-none');
+        if (currentShape == 'cross') {
+            currentShape = 'circle';
+            document.getElementById('p2').classList.remove('active-player');
+            document.getElementById('p1').classList.add('active-player');
+            document.getElementById(`circle-${id}`).classList.remove('d-none');
+        } else {
+            currentShape = 'cross';
+            document.getElementById('p1').classList.remove('active-player');
+            document.getElementById('p2').classList.add('active-player');
+            document.getElementById(`cross-${id}`).classList.remove('d-none');
+        }
+        fields[id] = currentShape;
+        checkWin();
     }
-    fields[id] = currentShape;
-    checkWin();
-}
 }
 
 
@@ -29,58 +29,70 @@ function checkWin() {
     if (fields[1] == fields[2] && fields[2] == fields[3] && fields[1]) {
         winner = fields[1];
         document.getElementById('horizontal-line1').classList.remove('d-none');
-        gameFinished();
+        gameFinished(winner);
     }
-    
+
     if (fields[4] == fields[5] && fields[5] == fields[6] && fields[4]) {
         winner = fields[4];
         document.getElementById('horizontal-line2').classList.remove('d-none');
-        gameFinished();
+        gameFinished(winner);
     }
-    
+
     if (fields[7] == fields[8] && fields[8] == fields[9] && fields[7]) {
         winner = fields[7];
         document.getElementById('horizontal-line3').classList.remove('d-none');
-        gameFinished();
+        gameFinished(winner);
     }
-    
+
     if (fields[1] == fields[4] && fields[4] == fields[7] && fields[4]) {
         winner = fields[4];
         document.getElementById('vertical-line1').classList.remove('d-none');
-        gameFinished();
+        gameFinished(winner);
     }
-    
+
     if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
         winner = fields[2];
         document.getElementById('vertical-line2').classList.remove('d-none');
-        gameFinished();
+        gameFinished(winner);
     }
-    
+
     if (fields[3] == fields[6] && fields[6] == fields[9] && fields[3]) {
         winner = fields[3];
         document.getElementById('vertical-line3').classList.remove('d-none');
-        gameFinished();
+        gameFinished(winner);
     }
-    
+
     if (fields[1] == fields[5] && fields[5] == fields[9] && fields[1]) {
         winner = fields[1];
         document.getElementById('diagonal-line1').classList.remove('d-none');
-        gameFinished();
+        gameFinished(winner);
     }
-    
+
     if (fields[3] == fields[5] && fields[5] == fields[7] && fields[3]) {
         winner = fields[3];
         document.getElementById('diagonal-line2').classList.remove('d-none');
-        gameFinished();
+        gameFinished(winner);
     }
 
 }
 
 
-function gameFinished() {
+function gameFinished(winner) {
     document.getElementById('tr1').classList.add('finishedGame');
     document.getElementById('tr2').classList.add('finishedGame');
     document.getElementById('tr3').classList.add('finishedGame');
     winnersound.play();
+    showWinner(winner);
+}
 
+function showWinner(winner) {
+    document.getElementById('winnerField').classList.remove('d-none');
+    document.getElementById('winnerField').innerHTML = `
+    <div class="winner-box"><span>${winner}</span><h1>hat das Spiel gewonnen</h1></div>
+    `;
+}
+
+
+function hideField() {
+    document.getElementById('winnerField').classList.add('d-none');
 }
